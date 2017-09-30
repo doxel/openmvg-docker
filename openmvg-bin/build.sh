644b85fa5b474f0f271ee032daec12c87a88fcd0
Rurik Bogdanov <rurik.bugdanov@alsenet.com>
2017-10-01 04:33:11 +0700

    openmvg-bin: add share/openMVG

--- openmvg-bin/build.sh
+++ openmvg-bin/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 [ -z "$DEBIAN_MIRROR" ] && DEBIAN_MIRROR=deb.debian.org
 
-chmod a+rwX bin
+chmod a+rwX bin share
 docker run --rm=true -v $(pwd):/mnt openmvg-build /mnt/strip_and_copy
 
 docker build \
   --build-arg debian_mirror=$DEBIAN_MIRROR \
   -t openmvg-bin \
   .
-
