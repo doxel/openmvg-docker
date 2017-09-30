aa445fd62860002f00f048091bb5ed48f992dabc
Rurik Bogdanov <rurik.bugdanov@alsenet.com>
2017-09-30 19:50:42 +0700

    remove temporary container after copying files from openmvg-build image

--- openmvg-bin/build.sh
+++ openmvg-bin/build.sh
@@ -2,7 +2,7 @@
 [ -z "$DEBIAN_MIRROR" ] && DEBIAN_MIRROR=deb.debian.org
 
 chmod a+rwX bin
-docker run -v $(pwd):/mnt openmvg-build /mnt/strip_and_copy
+docker run --rm=true -v $(pwd):/mnt openmvg-build /mnt/strip_and_copy
 
 docker build \
   --build-arg debian_mirror=$DEBIAN_MIRROR \
