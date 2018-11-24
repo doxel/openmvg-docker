6075b5b0cbf32862667d68b55643a476c266346c
Rurik Bugdanov <bugdanov@alsenet.com>
2018-11-24 10:59:35 +0100

    set default branch to master

--- openmvg-build/build.sh
+++ openmvg-build/build.sh
@@ -3,7 +3,7 @@
 
 docker build \
   --build-arg debian_mirror=$DEBIAN_MIRROR \
-  --build-arg branch=develop \
+  --build-arg branch=master \
   --build-arg BUILD_TESTS=OFF \
   --build-arg BUILD_EXAMPLES=ON \
   -t openmvg-build \
